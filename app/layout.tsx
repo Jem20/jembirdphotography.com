a9ae495c8d4f026a5b3911bd5c6bd750ca05e673
Jem20 <jemmm120@gmail.com>
2026-04-28 18:22:39 -0400

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { clsx } from 'clsx/lite';
 import {
@@ -98,6 +98,9 @@ export default function RootLayout({
       // Suppress hydration errors due to next-themes behavior
       suppressHydrationWarning
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+      </head>
       <body className={clsx(
         // Center on large screens
         '3xl:flex flex-col items-center',
